50f5c1446adf24523d97e680865d68e4338199c2
Maire Chew <mairechew@gmail.com>
2017-12-21 11:29:30 -0700

    added score

--- app.js
+++ app.js
@@ -40,15 +40,18 @@ var brickPadding = 10;
 var brickOffsetTop = 30;
 var brickOffsetLeft = 30;
 
+var score = 0;
+
+// INITIALIZE BRICKS
 // two-dimensional array (c)=brickColumns (r)=brickRows
 // setting x & y coordinates of each brick
 // bricks objects will be used for collision purposes later
 var bricks = [];
-for(c=0; c<brickColumnCount; c++) {
-    bricks[c] = [];
-    for(r=0; r<brickRowCount; r++) {
-        bricks[c][r] = { x: 0, y: 0 };
-    }
+for (c = 0; c < brickColumnCount; c++) {
+  bricks[c] = [];
+  for (r = 0; r < brickRowCount; r++) {
+    bricks[c][r] = { x: 0, y: 0, status: 1 };
+  }
 }
 
 const drawBall = () => {
@@ -70,28 +73,33 @@ const drawPaddle = () => {
 };
 
 const drawBricks = () => {
-    for(c=0; c<brickColumnCount; c++) {
-        for(r=0; r<brickRowCount; r++) {
-            // sets x-coordinate for individual bricks
-            var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
-            // sets y-coordinates for individual bricks
-            var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
-            bricks[c][r].x = brickX;
-            bricks[c][r].y = brickY;
-            ctx.beginPath();
-            ctx.rect(brickX, brickY, brickWidth, brickHeight);
-            ctx.fillStyle = "#0095DD";
-            ctx.fill();
-            ctx.closePath();
-        }
+  for (c = 0; c < brickColumnCount; c++) {
+    for (r = 0; r < brickRowCount; r++) {
+      // if status is 1, draw it, if not don't
+      if (bricks[c][r].status == 1) {
+        // sets x-coordinate for individual bricks
+        var brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+        // sets y-coordinates for individual bricks
+        var brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+        bricks[c][r].x = brickX;
+        bricks[c][r].y = brickY;
+        ctx.beginPath();
+        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+      }
     }
-}
+  }
+};
 
 const draw = () => {
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBricks();
   drawBall();
   drawPaddle();
+  drawScore();
+  collisionDetection();
   x += dx;
   y += dy;
   // Bounce off left and right (shouldReverseX)
@@ -101,16 +109,15 @@ const draw = () => {
 
   // Bounce off top and end game if hits bottom (shouldReverseY)
   // Bounce off paddle as well
-  if(y + dy < ballRadius) {
+  if (y + dy < ballRadius) {
+    dy = -dy;
+  } else if (y + dy > canvas.height - ballRadius) {
+    if (x > paddleX && x < paddleX + paddleWidth) {
       dy = -dy;
-  } else if(y + dy > canvas.height-ballRadius) {
-      if(x > paddleX && x < paddleX + paddleWidth) {
-          dy = -dy;
-      }
-      else {
-          alert("GAME OVER");
-          document.location.reload();
-      }
+    } else {
+      alert("GAME OVER");
+      document.location.reload();
+    }
   }
 
   // if keys are pressed - position paddle
@@ -119,25 +126,60 @@ const draw = () => {
   } else if (leftPressed && paddleX > 0) {
     paddleX -= 7;
   }
+
+  requestAnimationFrame(draw);
+
 };
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
-function keyDownHandler(e) {
+function keyDownHandler (e) {
   if (e.keyCode == 39) {
     rightPressed = true;
   } else if (e.keyCode == 37) {
     leftPressed = true;
   }
-}
+};
 
-function keyUpHandler(e) {
+function keyUpHandler (e) {
   if (e.keyCode == 39) {
     rightPressed = false;
   } else if (e.keyCode == 37) {
     leftPressed = false;
   }
-}
+};
 
-setInterval(draw, 100);
+const collisionDetection = () => {
+  for (c = 0; c < brickColumnCount; c++) {
+    for (r = 0; r < brickRowCount; r++) {
+      var b = bricks[c][r];
+      if (b.status == 1) {
+        // if collision happens, change status to 0 and it won't be painted on the screen
+        if (
+          x > b.x &&
+          x < b.x + brickWidth &&
+          y > b.y &&
+          y < b.y + brickHeight
+        ) {
+          dy = -dy;
+          b.status = 0;
+          score++;
+          // display message if all bricks are hit
+          if (score == brickRowCount * brickColumnCount) {
+            alert("YOU WIN, CONGRATULATIONS!");
+            document.location.reload();
+          }
+        }
+      }
+    }
+  }
+};
+
+const drawScore = () => {
+  ctx.font = "16px Arial";
+  ctx.fillStyle = "#0095DD";
+  ctx.fillText("Score: " + score, 8, 20);
+};
+
+draw();
